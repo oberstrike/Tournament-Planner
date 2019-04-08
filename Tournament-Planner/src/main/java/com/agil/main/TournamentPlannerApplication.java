5b6801597dd4b4dfcebc34a76ea95f6ecb6d04f4
JUERGMAR <JUERGMAR@D-W-D820593.fhhnet.stadt.hamburg.de>
2019-04-08 09:50:42 +0200

    Update

---
+++ Tournament-Planner/src/main/java/com/agil/main/TournamentPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.agil.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentPlannerApplication.class, args);
+	}
+
+}
