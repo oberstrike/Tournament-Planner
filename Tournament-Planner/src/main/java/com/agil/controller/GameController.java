9b3ce566345e3f059c6377f68be49a6d2b689efd
lorenjas-lokal <lorenjas-lokal@D-W-D818462.fhhnet.stadt.hamburg.de>
2019-04-10 15:31:13 +0200

    Templates, Gametypes

--- Tournament-Planner/src/main/java/com/agil/controller/GameController.java
+++ Tournament-Planner/src/main/java/com/agil/controller/GameController.java
@@ -5,10 +5,11 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.agil.utility.GameType;
 
-@Controller
+@RestController
 public class GameController {
 	
 	@GetMapping("/game/gametypes")
