f7aa2564c7690909664dfd2324ff39c6ff6ef9e3
JUERGMAR <JUERGMAR@D-W-D820593.fhhnet.stadt.hamburg.de>
2019-04-11 12:42:28 +0200

    Update

--- Tournament-Planner/src/main/java/com/agil/controller/GameController.java
+++ Tournament-Planner/src/main/java/com/agil/controller/GameController.java
@@ -1,13 +1,20 @@
 package com.agil.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.agil.model.Game;
+import com.agil.service.GameService;
 
 @Controller
 public class GameController {
+	
+	@Autowired
+	private GameService gameService;
 	
 	
 	@PostMapping("/game")
